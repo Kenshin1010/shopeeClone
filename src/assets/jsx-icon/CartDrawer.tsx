9a4d20cfdae090419f3b62acf674a255b18e98a2
anh.do <tuananh9h@gmail.com>
2024-04-14 00:16:59 +0700

    update home nav

--- src/assets/jsx-icon/CartDrawer.tsx
+++ src/assets/jsx-icon/CartDrawer.tsx
@@ -1,18 +1,22 @@
-
 export default function CartDrawer() {
   return (
-    <svg viewBox="0 0 26.6 25.6">
+    <svg viewBox="0 0 26.6 25.6" color="#fff" stroke="#fff"
+    width= "26px"
+    height= "26px"
+    font-size= "1.0625rem"
+    margin-right= ".625rem"
+    >
       <title>Shopping Cart Icon</title>
       <polyline
-        fill="none"
+        fill="#fff"
         points="2 1.7 5.5 1.7 9.6 18.3 21.2 18.3 24.6 6.1 7 6.1"
         stroke-linecap="round"
         stroke-linejoin="round"
         stroke-miterlimit="10"
         stroke-width="2.5"
       ></polyline>
-      <circle cx="10.7" cy="23" r="2.2" stroke="none"></circle>
-      <circle cx="19.7" cy="23" r="2.2" stroke="none"></circle>
+      <circle cx="10.7" cy="23" r="2.2" stroke="#fff"></circle>
+      <circle cx="19.7" cy="23" r="2.2" stroke="#fff"></circle>
     </svg>
   );
 }
--- src/pages/HomePage/components/HomeNav.tsx
+++ src/pages/HomePage/components/HomeNav.tsx
@@ -4,7 +4,6 @@ import {
   Button,
   Container,
   FormControl,
-  InputAdornment,
   Link,
   List,
   ListItem,
@@ -14,18 +13,18 @@ import {
   Stack,
   TextField,
   Toolbar,
-  Typography,
+  Typography
 } from "@mui/material";
 import { useMemo, useState } from "react";
 import shopeeLogo from "../../../assets/images/shopee2.svg";
 import ArrowDownIcon from "../../../assets/jsx-icon/ArrowDownIcon";
+import CartDrawer from "../../../assets/jsx-icon/CartDrawer";
 import HelpCenterIcon from "../../../assets/jsx-icon/HelpCenterIcon";
 import NotificationIcon from "../../../assets/jsx-icon/NotificationIcon";
 import SearchIcon from "../../../assets/jsx-icon/SearchIcon";
 import ShopeeAvatar from "../../../assets/jsx-icon/ShopeeAvatar";
 import StardustPopover from "../../../assets/jsx-icon/StardustPopover";
 import StardustPopoverTarget from "../../../assets/jsx-icon/StardustPopoverTarget";
-import { lineHeight, minWidth } from "@mui/system";
 
 const options = ["Trong Nhà Sách Online", "Trong Shopee"];
 function HomeNav() {
@@ -282,7 +281,7 @@ function HomeNav() {
       "& .MuiInputBase-root:focus": { outline: "none" },
 
       "& .MuiOutlinedInput-root": { lineHeight: "20px", fontSize: ".875rem" },
-      "& .css-154xyx0-MuiInputBase-root-MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+      "& .css-md26zr-MuiInputBase-root-MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
         {
           borderColor: "rgba(0, 0, 0, 0.87)",
           borderWidth: "1px",
@@ -316,6 +315,7 @@ function HomeNav() {
     maxWidth: "9.375rem",
     position: "relative",
     userSelect: "none",
+    borderRadius: "none",
   });
 
   const shopeeSearchBarSelectorTriggerStyles = () => ({
@@ -342,6 +342,64 @@ function HomeNav() {
     display: "block",
     minHeight: "1.2em",
     padding: "0px 2px 1px",
+    whiteSpace: "nowrap",
+  });
+
+  const stackLinkProductStyles = () => ({
+    position: "relative",
+    // flexWrap: "nowrap",
+    flexWrap: "wrap",
+    fontSize: ".75rem",
+    fontWeight: "300",
+    height: "1.5rem",
+    lineHeight: "1.5rem",
+    marginTop: ".1875rem",
+    // overflowY: "visible",
+    overflowY: "clip",
+  });
+
+  const LinkProductStyles = () => ({
+    "&:first-child": {
+      // marginLeft: "0",
+    },
+    // color: "rgba(0, 0, 0, .87)",
+    // color: "rgba(0, 0, 0, .54)",
+    color: "hsla(0, 0%, 100%, .9)",
+    // display: "block",
+    height: ".875rem",
+    lineHeight: ".875rem",
+    marginBottom: ".3125rem",
+    marginLeft: "13px",
+    marginTop: ".3125rem",
+    outline: "0",
+    overflow: "visible",
+    position: "relative",
+    textDecoration: "none",
+    whiteSpace: "nowrap",
+  });
+
+  const cartWrapperStyles = () => ({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: "1",
+    margin: "0 10px",
+    paddingBottom: "5px",
+  });
+
+  const cartContainerStyles = () => ({
+    padding: "10px 0",
+  });
+
+  const cartDrawerStyles = () => ({
+    alignItems: "center",
+    display: "flex",
+    cursor: "pointer",
+    margin: "0 0 0 5px",
+    outline: "0",
+    overflow: "visible",
+    position: "relative",
+    textDecoration: "none",
     whiteSpace: "nowrap",
   });
 
@@ -597,15 +655,16 @@ function HomeNav() {
               />
             </Box>
           </Link>
-          <Box
+          <Stack
             sx={{
               display: "flex",
-              flexDirection: "row",
+              flexDirection: "column",
               alignItems: "center",
               justifyContent: "flex-start",
               position: "relative",
-              width: "840px",
+              visibility: "visible",
               height: "41px",
+              width: "840px",
             }}
           >
             <FormControl
@@ -712,140 +771,209 @@ function HomeNav() {
                 <SearchIcon />
               </Button>
             </FormControl>
-            <Box>
-              {/* <Box>
-                <Link
-                  aria-hidden="false"
-                  href="/search?keyword=v%C3%B5%20ph%E1%BB%A5c%20karate%20rikaido"
-                >
-                  Võ Phục Karate Rikaido
-                </Link>
+            <Stack flex={"1"} width={"100%"}>
+              <Box sx={stackLinkProductStyles}>
                 <Link
                   aria-hidden="false"
                   href="/search?keyword=kotoro%20wa"
+                  sx={LinkProductStyles}
                 >
                   Kotoro Wa
                 </Link>
                 <Link
                   aria-hidden="false"
                   href="/search?keyword=v%C3%B5%20ph%E1%BB%A5c%20karate"
+                  sx={LinkProductStyles}
                 >
                   Võ Phục Karate
                 </Link>
                 <Link
                   aria-hidden="false"
                   href="/search?keyword=m%C3%A0n%20h%C3%ACnh%20dell%20n5050"
+                  sx={LinkProductStyles}
                 >
                   Màn Hình DELL N5050
                 </Link>
                 <Link
                   aria-hidden="false"
                   href="/search?keyword=ai%20tr%C3%AD%20tu%E1%BB%87%20nh%C3%A2n%20t%E1%BA%A1o"
+                  sx={LinkProductStyles}
                 >
                   Ai Trí Tuệ Nhân Tạo
                 </Link>
-                <Link aria-hidden="false" href="/search?keyword=naraka%20bladepoint">
-                    Naraka Bladepoint
+                <Link
+                  aria-hidden="false"
+                  href="/search?keyword=naraka%20bladepoint"
+                  sx={LinkProductStyles}
+                >
+                  Naraka Bladepoint
                 </Link>
-                <Link aria-hidden="false" href="/search?keyword=b%C3%BAt%20th%C6%B0%20ph%C3%A1p">
-                Bút Thư Pháp
-                </Link>
-                <Link aria-hidden="false" href="/search?keyword=alone%204">
-                Alone 4
+                <Link
+                  aria-hidden="false"
+                  href="/search?keyword=b%C3%BAt%20th%C6%B0%20ph%C3%A1p"
+                  sx={LinkProductStyles}
+                >
+                  Bút Thư Pháp
                 </Link>
                 <Link
                   aria-hidden="false"
                   href="/search?keyword=s%C3%A1ch%20thanh%20l%C3%BD"
+                  sx={LinkProductStyles}
                 >
                   Sách Thanh Lý
                 </Link>
-                <Link aria-hidden="false" href="/search?keyword=m%C3%B4%20h%C3%ACnh%20l%C3%BD%20c%E1%BA%A3nh%20lung">
-                Mô Hình Lý Cảnh Lung
+                <Link
+                  aria-hidden="false"
+                  href="/search?keyword=m%C3%B4%20h%C3%ACnh%20l%C3%BD%20c%E1%BA%A3nh%20lung"
+                  sx={LinkProductStyles}
+                >
+                  Mô Hình Lý Cảnh Lung
                 </Link>
                 <Link
                   aria-hidden="false"
                   href="/search?keyword=simon"
+                  sx={LinkProductStyles}
                 >
                   Simon
                 </Link>
                 <Link
                   aria-hidden="false"
                   href="/search?keyword=%C4%91ai%20karate"
+                  sx={LinkProductStyles}
                 >
                   Đai Karate
                 </Link>
                 <Link
                   aria-hidden="true"
                   href="/search?keyword=kanji%20look%20and%20learn"
+                  sx={LinkProductStyles}
                 >
                   Kanji Look And Learn
                 </Link>
                 <Link
                   aria-hidden="true"
                   href="/search?keyword=v%C6%B0%C6%A1ng%20kh%C3%AD%20r%E1%BB%ABng%20s%C3%A2u"
+                  sx={LinkProductStyles}
                 >
                   Vương Khí Rừng Sâu
                 </Link>
-                <Link aria-hidden="true" href="/search?keyword=sticker">
+                <Link
+                  aria-hidden="true"
+                  href="/search?keyword=sticker"
+                  sx={LinkProductStyles}
+                >
                   Sticker
                 </Link>
-                <Link aria-hidden="true" href="/search?keyword=qu%E1%BA%A1t%20t%E1%BA%A3n%20nhi%E1%BB%87t%20laptop">
-                    Quạt Tản Nhiệt Laptop
+                <Link
+                  aria-hidden="true"
+                  href="/search?keyword=qu%E1%BA%A1t%20t%E1%BA%A3n%20nhi%E1%BB%87t%20laptop"
+                  sx={LinkProductStyles}
+                >
+                  Quạt Tản Nhiệt Laptop
                 </Link>
                 <Link
                   aria-hidden="true"
                   href="/search?keyword=n%C6%B0%E1%BB%9Bc%20hoa"
+                  sx={LinkProductStyles}
                 >
                   Nước Hoa
                 </Link>
                 <Link
                   aria-hidden="true"
                   href="/search?keyword=n%C3%B3i%20ti%E1%BA%BFng%20anh%20nh%C6%B0%20gi%C3%B3"
+                  sx={LinkProductStyles}
                 >
                   Nói Tiếng Anh Như Gió
                 </Link>
-                <Link aria-hidden="true" href="/search?keyword=lovito">
+                <Link
+                  aria-hidden="true"
+                  href="/search?keyword=lovito"
+                  sx={LinkProductStyles}
+                >
                   Lovito
                 </Link>
-                <Link aria-hidden="true" href="/search?keyword=body%20mist">
+                <Link
+                  aria-hidden="true"
+                  href="/search?keyword=body%20mist"
+                  sx={LinkProductStyles}
+                >
                   Body Mist
                 </Link>
                 <Link
                   aria-hidden="true"
+                  href="/search?keyword=alone%204"
+                  sx={LinkProductStyles}
+                >
+                  Alone 4
+                </Link>
+                <Link
+                  aria-hidden="true"
                   href="/search?keyword=s%C3%A1ch%20vi%E1%BB%87t"
+                  sx={LinkProductStyles}
                 >
                   Sách Việt
                 </Link>
                 <Link
                   aria-hidden="true"
                   href="/search?keyword=tr%E1%BA%A5n%20qu%E1%BB%91c%20th%E1%BA%A7n%20k%C3%ADch%20ho%C3%A0n%20m%E1%BB%B9"
+                  sx={LinkProductStyles}
                 >
                   Trấn Quốc Thần Kích Hoàn Mỹ
                 </Link>
                 <Link
                   aria-hidden="true"
                   href="/search?keyword=m%C3%B4%20h%C3%ACnh%20t%E1%BA%A7n%20th%E1%BB%9Di%20minh%20nguy%E1%BB%87t"
+                  sx={LinkProductStyles}
                 >
                   Mô Hình Tần Thời Minh Nguyệt
                 </Link>
-              </Box> */}
-            </Box>
-          </Box>
-          {/* <Box>
-            <Box>
-              <Box>
-                <Box>
-                  <Link href="/cart">
+              </Box>
+            </Stack>
+          </Stack>
+          <Box sx={cartWrapperStyles}>
+            <Box position={"relative"}>
+              <Box role="button">
+                <Box sx={cartContainerStyles}>
+                  <Link href="/cart" sx={cartDrawerStyles}>
                     <CartDrawer />
-                    <Typography>
+                    <Typography
+                      sx={{
+                        transformOrigin: "0px 0px",
+                        fontSize: "1.0625rem",
+                        marginRight: ".625rem",
+                        display: "none",
+                      }}
+                    >
                       Shopping Cart number of items in cart 144
                     </Typography>
-                    <Box aria-hidden="true">144</Box>
+                    <Box
+                      aria-hidden="true"
+                      sx={{
+                        left: "-1.25rem",
+                        top: "-.6875rem",
+                        backgroundColor: "#fff",
+                        border:
+                          ".125rem solid var(--brand-primary-color, #ee4d2d)",
+                        borderRadius: "2.75rem",
+                        color: "var(--brand-primary-color, #ee4d2d)",
+                        height: "1rem",
+                        lineHeight: "1.2em",
+                        marginRight: "-.875rem",
+                        minWidth: ".6875rem",
+                        padding: "0 .3125rem",
+                        position: "relative",
+                        textAlign: "center",
+                        // left: "-1.125rem",
+                        // top: "-.5rem",
+                      }}
+                    >
+                      144
+                    </Box>
                   </Link>
                 </Box>
               </Box>
             </Box>
-          </Box> */}
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
