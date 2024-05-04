2be2c5f50b3810a98df10cdb483951b3de3777a0
anh.do <tuananh9h@gmail.com>
2024-05-04 17:51:00 +0700

    redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,12 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["js-big-decimal"],
   },
+  server: {
+    port: 3000,
+  },
+  css: {
+    devSourcemap: true,
+  },
   resolve: {
     alias: {
       src: path.resolve(__dirname, "/src"),
